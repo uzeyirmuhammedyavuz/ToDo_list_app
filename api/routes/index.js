5f8f1165fff70ab86467828677546c7e5f08f524
üzeyir <barbaross_61@hotmail.com>
2024-09-02 18:42:04 +0300

    static route added

---
+++ api/routes/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+
+let routes = fs.readdirSync(__dirname);
+
+for (let route of routes) {
+  if (route.includes(".js") && route != "index.js") {
+    router.use("/" + route.replace(".js", ""), require("./" + route));
+  }
+}
+
+module.exports = router;
