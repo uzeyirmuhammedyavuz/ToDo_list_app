9277c8d5bc991278a526f44c8c02fc4a5f39abd1
üzeyir <barbaross_61@hotmail.com>
2024-09-02 18:44:43 +0300

    db connection auth added

--- api/app.js
+++ api/app.js
@@ -1,11 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const routes = require("./routes");
+const bodyParser = require("body-parser");
+const sequelize = require("./db/db");
 const port = 3000;
 
-app.get("/api", function (req, res) {
-  res.send("Hello World");
-});
+app.use(express.json());
+app.use(bodyParser.json());
 
-app.listen(port, () => {
-  console.log(`app running on port ${port}`);
+app.use("/api", routes);
+
+app.listen(port, async () => {
+  try {
+    console.log(`app running on port ${port}`);
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 });
