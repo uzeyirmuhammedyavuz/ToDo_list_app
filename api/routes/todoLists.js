88b5317cc43763fd311dc4622bc94511c5b6ed7e
üzeyir <barbaross_61@hotmail.com>
2024-09-04 00:36:54 +0300

    todolists rename

--- api/routes/todoLists.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const todoListController = require("../controllers/todoListController");
-const authMiddleware = require("../middlewares/authMiddleware")
-
-router.get("/add",authMiddleware, todoListController.addTodoList);
-
-module.exports = router;
